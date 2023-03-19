2c3eaf812dce7566310ae1b00646fb21cbbdbf85
Adeoye Adeyinka <111874994+Hardey1774@users.noreply.github.com>
2023-03-19 21:26:50 +0100

    Update jenkins-ubuntu-installation.sh

--- JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
+++ JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
@@ -16,3 +16,15 @@ sudo dnf install jenkins
 sudo systemctl daemon-reload  # To Register the Jenkins service 
 sudo systemctl start jenkins
 systemctl status jenkins
+
+
+or
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo systemctl status jenkins

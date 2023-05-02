27904875a7a5f8a9ace196e6ce32833fe189fc90
Adeoye Adeyinka <111874994+Hardey1774@users.noreply.github.com>
2023-05-02 22:08:16 +0100

    Update Jenkins-redhat8-install28.sh

--- JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
+++ JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
@@ -6,13 +6,13 @@
 sudo timedatectl set-timezone America/New_York
 sudo hostnamectl set-hostname jenkins
 sudo yum install wget -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install java-11-openjdk -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
 # Add required dependencies for the jenkins package
-sudo yum install java-11-openjdk -y
-sudo yum install jenkins -y
+ yum install fontconfig java-11-openjdk
+  yum install jenkins -y
 sudo systemctl daemon-reload
 # start jenkins
 # Start Jenkins
